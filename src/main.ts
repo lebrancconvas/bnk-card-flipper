70b878fd1522938c2a1bda04ee3a647105f9adc6
lebrancconvas <lebrancconvas@gmail.com>
2025-06-29 14:41:18 +0700

    ✨ feat: import old code to the remastered project.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,56 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const cards = document.querySelectorAll('.memory-card')
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let firstcard , secondcard
+let hasFlippedCard = false
+let lockBoard = false
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function flipCard() {
+    if(lockBoard) return
+    if(this === firstcard) return
+
+    this.classList.add('flip')
+    if(!hasFlippedCard) {
+        hasFlippedCard = true
+        firstcard = this
+        return
+    }
+
+    secondcard = this
+    checkForMatch()
+}
+
+function checkForMatch() {
+    let isMatched = firstcard.dataset.framework === secondcard.dataset.framework
+    isMatched ? matchCard() : unmatchCard()
+}
+
+function matchCard() {
+    firstcard.removeEventListener('click', flipCard)
+    secondcard.removeEventListener('click', flipCard)
+    resetBoard()
+}
+
+function unmatchCard() {
+    lockBoard = true
+    setTimeout(() => {
+        firstcard.classList.remove('flip')
+        secondcard.classList.remove('flip')
+        resetBoard()
+    }, 1000)
+}
+
+function resetBoard() {
+    hasFlippedCard = false
+    lockBoard = false
+    firstcard = null
+    secondcard = null
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12)
+        card.style.order = randomPos
+    })
+})()
+
+cards.forEach(card => card.addEventListener('click',flipCard))
